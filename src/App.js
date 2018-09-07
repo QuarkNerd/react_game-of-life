8d6832dacc24361708e4715f35a9a795104211e2
Gurveer Arora <Garora1410@yahoo.co.uk>
2018-09-07 13:47:31 +0100

    Make Game work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Grid from './Grid'
 
 class App extends Component {
-  render() {
+  grid = React.createRef()
+  state = { interval: 1000 }
+
+  componentDidMount () {}
+
+  tick = () => {
+    this.grid.current.tick()
+  }
+
+  start = () => {
+    if (!this.timer) {
+      this.timer = setInterval(this.tick, this.state.interval)
+    }
+  }
+
+  stop = () => {
+    clearInterval(this.timer)
+    this.timer = null
+  }
+
+  intervalChange = e => {
+    const interval = Number(e.target.value) * 1000
+    if (!isNaN(interval) && interval >= 0) {
+      this.setState({ interval })
+    }
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Grid numRows={9} numColumns={9} ref={this.grid} />
+        <button onClick={this.start}> Start </button>
+        <button onClick={this.tick}> Tick </button>
+        <button onClick={this.stop}> Stop </button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
+
+// const rootElement = document.getElementById("root");
+/// // size is number of rows , numbr of columns
+/// /ReactDOM.render(<Square/>, rootElement);
+// ReactDOM.render(<Grid numRows={9} numColumns={9}/>, rootElement);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App numRows={9} numColumns={9} />, rootElement)
+registerServiceWorker()
